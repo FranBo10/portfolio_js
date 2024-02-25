708b123bc79f6eb2f1a9300185a7c79ba81a08e3
FranBo10 <franbo10pro@gmail.com>
2024-02-25 16:19:16 +0100

    Modificacion Projets

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -2,26 +2,27 @@ import { Button, IconButton } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
+const ProjectItem = ({ title, description, backgroundImg, projectUrl }) => {
   return (
-    <div class="relative flex items-center justify-center h-auto shadow-xl shadow-gray-400 rounded-xl p-1 group hover:bg-gradient-to-r from-cyan-500 text- to-teal-500 cursor-pointer hover:scale-105 ease-in duration-100">
+    <div class="relative flex items-center justify-center h-auto shadow-xl shadow-gray-400 rounded-xl p-1 group hover:bg-gradient-to-r from-gray-700 text- to-gray-700 cursor-pointer hover:scale-105 ease-in duration-300">
       <img
         class="rounded-xl group-hover:opacity-10 h-full w-full"
         src={backgroundImg}
         alt=""
       />
       <div class="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 class="text-2xl text-white tracking-wider text-center"></h3>
-        <p class="pb-4 pt-2 text-white text-center text-2xl">{title}</p>
+        <h3 class="text-2xl text-teal-500 tracking-wider text-center"></h3>
+        <p class="pb-4 pt-2 text-teal-500 text-center text-2xl">{title}</p>
+        <p class="pb-4 pt-2 text-teal-500 text-center text-xl">{description}</p>
         <div class="flex">
           <Link
             to={projectUrl}
             target="_blank"
-            class="px-8 py-2 mt-4 mr-8 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-black-500 text- to-slate-500 text-gray-100 hover:scale-125 ease-in duration-100"
+            class="px-8 py-2 mt-4 mr-8 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-black-500 text- to-slate-500 text-teal-500 hover:scale-125 ease-in duration-100 border border-teal-500"
           >
             Demo
           </Link>
-          <button class="px-8 py-2 mt-4 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-black-500 text- to-slate-500 text-gray-100 hover:scale-125 ease-in duration-100">
+          <button class="px-8 py-2 mt-4 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-black-500 text- to-slate-500 text-teal-500 hover:scale-125 ease-in duration-100 border border-teal-500">
             Code
           </button>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import todolist from "../../img/todolist.png";
 import pokedex from "../../img/pokedex.jpg";
 import pokedex2 from "../../img/pokedex2.png";
-import web4 from "../../img/web4.png";
+import karnak from "../../img/karnak.png";
 import web5 from "../../img/web5.png";
 import freetourgo from "../../img/freetourgo.png";
 import ProjectItem from "../ProjectItem/ProjectItem";
@@ -16,26 +16,35 @@ const Projects = () => {
         </p>
         <h2 class="py-4 dark:text-gray-100">Voici mes projets</h2>
         <div class="grid md:grid-cols-2 gap-8">
-          <ProjectItem
-            title="ToDoList"
-            backgroundImg={todolist}
-            projectUrl="https://franbo10.github.io/todolist_js/"
+        <ProjectItem
+            title="Karnak Porcellanato"
+            description="Premier projet HTML / CSS (2020)"
+            backgroundImg={karnak}
+            projectUrl="https://franbo10.github.io/karnak/"
           />
           <ProjectItem
             title="FreetourGo"
+            description="Application web PHP Symfony / Javascript (2023)"
             backgroundImg={freetourgo}
             projectUrl="https://freetourgo.com"
           />
           <ProjectItem
             title="Pokedex"
+            description="Connexion à PokeAPI, affiche et système de recherche de Pokemons"
             backgroundImg={pokedex}
             projectUrl="https://franbo10.github.io/pokedex_js/"
           />
-          <ProjectItem
+          {/* <ProjectItem
             title="PokeAPI"
             backgroundImg={pokedex2}
             projectUrl="https://franbo10.github.io/pokemon_js/"
           />
+          
+          <ProjectItem
+            title="ToDoList"
+            backgroundImg={todolist}
+            projectUrl="https://franbo10.github.io/todolist_js/"
+          /> */}
         </div>
       </div>
     </div>
